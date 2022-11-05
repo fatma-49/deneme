4aaae79e34c956f57691ff0cf63284096fe3c0cd
Fatma Görür <fkumgorur@gmail.com>
2022-11-05 18:06:47 +0300

    v1

---
+++ src/main/java/org/example/h.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class h {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
